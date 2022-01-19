75a4d0726e4a0862629d08967e38415f300e33f1
Jejkukany <57088886+jejkukany@users.noreply.github.com>
2022-01-19 16:16:34 +0000

    Adding a web

--- items.py
+++ items.py
@@ -60,15 +60,28 @@ ps1 = """Hello.
   So take a seat and enjoy your own prediction."""
 #1st set of questions
 #s1 = "Your close friends went to a nightclub and you don'tave nothing to do"
-s1 = "Your close friend want to go to the party tonight and asked you, if you want to go with him"
+s1 = "Your close friend wants to go to the party tonight and asked you, if you want to go with him"
 q1 = """You have nothing planed. what will you do??
  """
 a1 = "a) I will stay at home."
 a2 = "b) I will go with them but only drink non alkoholic drinks."
 a3 = "c) I will go there and drink and party with them."
-#2ond set of questions
-s2 = "You are going to the bus stop, and you see a wallet laying on the floor."
-q2 = "You are in extreme hurry.  what will you do?"
-a5 = "i will leave it there."
-a4 = "i will take it and pretend that it´s mine."
-a6 = "i will just say someone to do something with it."
+#2nd set of questions
+s2 = """You are going to the bus stop, and you see a wallet laying on the floor."""
+q2 = """You are in extreme hurry.  what will you do?
+"""
+a5 = "b) i will leave it there."
+a4 = "a) i will take it and pretend that it´s mine."
+a6 = "c) i will just tell someone to do something with it."
+#3rd set of questions
+s3 = """Your friend called you and told you that he wot two tickets to the hockey match, witch is tommorow at 2 pm. He asked you if you want to go with him. You are going to the cinema with your other friend."""
+q3 = """What will you tell him?"""
+a7 = "a) Sorry, i am going to the cinema tommorow."
+a8 = "b) Ok, i will cancel my other plans and go with you."
+a9 = "c) Sorry, i am having family dinner"
+#4rd set of question
+s4 = ""
+q4 = ""
+a10 = ""
+a11 = ""
+a12 = ""
--- main.py
+++ main.py
@@ -40,7 +40,7 @@ else:
   print(name)
   print(error)
   print("Problem:")
-  print('Valid input error')
+  print('Unvalid input error')
   time.sleep(999999)
   
 clear()
@@ -50,7 +50,7 @@ clear()
 print(name)
 print(s2)
 wait()
-print(q3)
+print(q2)
 wait()
 print(a4)
 wait()
@@ -60,3 +60,54 @@ print(a6)
 wait()
 print("")
 answer = input('what will you choose? ')
+
+if answer.upper() == "A":
+  score = int(score) + 3
+elif answer.upper() == "B":
+  score = int(score) + 1
+elif answer.upper() == "C":
+  score = int(score) + 2
+else:
+  clear()
+  print(name)
+  print(error)
+  print("Problem:")
+  print('Valid input error')
+  time.sleep(999999)
+
+clear()
+qprogbar()
+clear()
+
+print(name)
+print(s3)
+wait()
+print(q3)
+wait()
+print(a7)
+wait()
+print(a8)
+wait()
+print(a9)
+wait()
+print("")
+answer = input('what will you choose? ')
+
+if answer.upper() == "A":
+  score = int(score) + 2
+elif answer.upper() == "B":
+  score = int(score) + 1
+elif answer.upper() == "C":
+  score = int(score) + 3
+else:
+  clear()
+  print(name)
+  print(error)
+  print("Problem:")
+  print('Valid input error')
+  time.sleep(999999)
+
+clear()
+qprogbar()
+clear()
+
